f3a255a3a6e9f5ddd19874b5f1e67fe8506f400b
mgoulley <mattgoulley@gmail.com>
2024-05-14 22:53:27 +0200

    fix settings

--- djangoepgp/settings-prd.py
+++ djangoepgp/settings-prd.py
@@ -108,7 +108,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            "read_default_file": BASE_DIR / "mysql.conf",
+            "read_default_file": "./mysql.conf",
         },
     }
 }
