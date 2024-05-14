da118b56887dbb1ae5f5a20ec8fef43682f1d804
mgoulley <mattgoulley@gmail.com>
2024-05-14 23:59:19 +0200

    deploy nginx

--- deploy-nginx.sh
+++ deploy-nginx.sh
@@ -2,4 +2,4 @@
 set -x
 
 cat nginx.conf > /etc/nginx/sites-available/lbdb_nginx.conf
-sudo ln -s /etc/nginx/sites-available/lbdb_nginx.conf /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/lbdb_nginx.conf /etc/nginx/sites-enabled/
