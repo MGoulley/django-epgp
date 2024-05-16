f3c78ed6914b1abe88cb45378179352078cd4434
mgoulley <mattgoulley@gmail.com>
2024-05-16 23:51:17 +0200

    fix connexion

--- account/forms.py
+++ account/forms.py
@@ -1,26 +1,18 @@
 # -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
 
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username",                
+                "placeholder" : "Utilisateur",                
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",                
+                "placeholder" : "Mot de passe",                
                 "class": "form-control"
             }
         ))
--- account/views.py
+++ account/views.py
@@ -1,33 +1,12 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render, redirect
-from django.template import loader
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import *
-
-def index(request):
-    data = {}
-    return render(request, "index.html", context=data)
-
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
-
-        load_template = request.path.split('/')[-1]
-        template = loader.get_template('pages/' + load_template)
-        return HttpResponse(template.render(context, request))
-
-    except:
-
-        template = loader.get_template( 'pages/error-404.html' )
-        return HttpResponse(template.render(context, request))
 
 def login_view(request):
     form = LoginForm(request.POST or None)
 
-    msg = 'Sign in with credentials'
+    msg = 'Se connecter'
 
     if request.method == "POST":
         if form.is_valid():
@@ -38,28 +17,8 @@ def login_view(request):
                 login(request, user)
                 return redirect("/")
             else:
-                msg = 'Invalid credentials'
+                msg = 'Mot de passe invalide'
         else:
-            msg = 'Error validating the form'
+            msg = 'Impossible de se connecter'
 
     return render(request, "accounts/login.html", {"form": form, "msg" : msg})
-
-def register_user(request):
-
-    msg = 'Add your credentials'
-
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            request.session["user_id"] = user.id
-            return redirect("/accounts/login/")
-        else:
-            msg = 'Form is not valid'
-    else:
-        form = SignUpForm()
-
-    return render(request, "accounts/register.html", {"form": form, "msg" : msg})
