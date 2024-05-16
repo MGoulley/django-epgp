73f5375d9264355923c7342627e9507771d24a44
mgoulley <mattgoulley@gmail.com>
2024-05-16 19:33:50 +0200

    Fix this

--- start.sh
+++ start.sh
@@ -4,5 +4,9 @@ set -x
 source .venv/bin/activate
 
 python3.11 manage.py check --deploy
-#python3.11 manage.py runserver 0.0.0.0:80
+python3.11 manage.py runserver 0.0.0.0:80
 uwsgi --ini lbdb_uwsgi.ini
+
+
+
+TODO ACL pour github + www.data
