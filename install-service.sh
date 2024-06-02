2a86fe328e0745d2e1954a6bad297acd560d46b7
Matthias Goulley <mattgoulley@gmail.com>
2024-06-02 17:09:43 +0200

    Change directory

--- install-service.sh
+++ install-service.sh
@@ -2,5 +2,6 @@
 set -x
 
 echo "Copie dans systemd"
-cp /opt/lbdb/django/lbdb-django.service /etc/systemd/system/
+cp /opt/lbdb/django/lbdb-django.service ~/.config/systemd/user/
 systemctl --user enable lbdb-django.service
+systemctl --user daemon-reload
